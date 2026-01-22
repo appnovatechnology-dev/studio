9493027e7edeba61838761686b3e4023e24cfb49
App Nova <appnova.technology@gmail.com>
2026-01-22 23:17:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1743463009-d3cc4",
+  "appId": "1:355253046490:web:fce24e0d098560905ea9e3",
+  "apiKey": "AIzaSyCzss5uixi-gYWQLlxSAJo6Ist6fXYyXR0",
+  "authDomain": "studio-1743463009-d3cc4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "355253046490"
+};
